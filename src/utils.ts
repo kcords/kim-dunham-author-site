d0775416225f3553cfda34f58d24bd259e54667c
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2024-08-17 08:16:29 -0700

    Removes errored file

--- src/utils.ts
+++
@@ -1,4 +0,0 @@
-type ClassState = { [key: string]: boolean }
-
-
-export const compileClassList = (classState: ClassState): string => Object.entries(classState).reduce((classListStr, [class, state]) => state ? classListStr : classListStr, '').trim()
