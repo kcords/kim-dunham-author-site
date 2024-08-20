69fd688c4a92cf89b0e632c1370abdacfe4481a9
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2024-08-19 19:58:48 -0700

    Adds skeleton for NewsletterFeed

--- src/pages/Newsletter/Newsletter.tsx
+++ src/pages/Newsletter/Newsletter.tsx
@@ -1,5 +1,12 @@
+import { NewsletterFeed } from "../../components/Sections/NewsletterFeed";
+import { NewsletterSignup } from "../../components/Sections/NewsletterSignup/NewsletterSignup";
 import classes from "./Newsletter.module.css";
 
 export const Newsletter = () => {
-  return <main className={classes.contentBody}></main>;
+  return (
+    <main className={classes.contentBody}>
+      <NewsletterSignup />
+      <NewsletterFeed />
+    </main>
+  );
 };
