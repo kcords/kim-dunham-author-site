1118c0755665fbf044774e361adb264029bcec3e
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2024-08-20 10:59:48 -0700

    Adds socials links to contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,5 +1,11 @@
+import { SocialIconSize } from "../../consts";
+import { Socials } from "../../components/Socials";
 import classes from "./Contact.module.css";
 
 export const Contact = () => {
-  return <main className={classes.contentBody}></main>;
+  return (
+    <main className={classes.contentBody}>
+      <Socials size={SocialIconSize.Large} showLabel />
+    </main>
+  );
 };
