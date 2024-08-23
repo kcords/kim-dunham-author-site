f161a9d14f47153bc34ca9dbabe4468e2f3b4159
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2024-08-23 13:44:16 -0700

    Adds AboutContent sections to About page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,13 @@
+import { AboutContent } from "../../components/Sections/AboutContent";
+import { about } from "../../strings";
 import classes from "./About.module.css";
 
 export const About = () => {
-  return <main className={classes.contentBody}></main>;
+  return (
+    <main className={classes.contentBody}>
+      <AboutContent heading={about.storiesHeading} text={about.storiesText} />
+      <AboutContent heading={about.forestsHeading} text={about.forestsText} />
+      <AboutContent heading={about.writingHeading} text={about.writingText} />
+    </main>
+  );
 };
