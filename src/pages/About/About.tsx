696c8b0a3265de81f981281158e875dc43d01d86
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2024-08-23 20:30:27 -0700

    Adds Photos to About page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,13 +1,27 @@
 import { AboutContent } from "../../components/Sections/AboutContent";
+import { Photo } from "../../components/Photo";
 import { about } from "../../strings";
 import classes from "./About.module.css";
+import forestStump from "../../assets/images/forest_stump_square.jpg";
+import kDunhamCoast from "../../assets/images/kdunham_oregon_coast_square.jpg";
+import kodaWriting from "../../assets/images/k_koda_square.jpg";
 
 export const About = () => {
   return (
     <main className={classes.contentBody}>
-      <AboutContent heading={about.storiesHeading} text={about.storiesText} />
-      <AboutContent heading={about.forestsHeading} text={about.forestsText} />
-      <AboutContent heading={about.writingHeading} text={about.writingText} />
+      <AboutContent heading={about.storiesHeading} text={about.storiesText}>
+        <Photo src={kDunhamCoast} alt={""} height="300" width="300" />
+      </AboutContent>
+      <AboutContent
+        heading={about.forestsHeading}
+        text={about.forestsText}
+        alignInsert={AboutContent.AlignInsert.Right}
+      >
+        <Photo src={forestStump} alt={""} height="300" width="300" />
+      </AboutContent>
+      <AboutContent heading={about.writingHeading} text={about.writingText}>
+        <Photo src={kodaWriting} alt={""} height="300" width="300" />
+      </AboutContent>
     </main>
   );
 };
