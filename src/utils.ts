9af07952fcafd68fd7b1cd308d11a38f0f15fb3d
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2024-08-25 20:40:57 -0700

    Adds formatDateStr util fn

--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,11 @@ export const compileClassList = (...args: string[]): string =>
       ""
     )
     .trim();
+
+export const formatDateStr = (dateStr: string): string =>
+  new Date(dateStr.replace(" ", "T")).toLocaleDateString("en-us", {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
