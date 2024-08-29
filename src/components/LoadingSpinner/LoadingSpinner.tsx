208e36470c348a64a1a909136adfaad1cee9a946
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2024-08-28 22:34:45 -0700

    Adds label and role to loading spinner

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,5 +1,8 @@
 import LoadingSvg from '../../assets/icons/infinite-spinner.svg'
 
+const ARIA_LABEL = 'Loading content'
+const ROLE = "progressbar"
+
 interface LoadingSpinnerProps {
   size?: Size
 }
@@ -10,8 +13,8 @@ enum Size {
   LG = '200'
 }
 
-export const LoadingSpinner = ({size = Size.MD}: LoadingSpinnerProps) => {
-  return (<div><LoadingSvg height={size} width={size}/></div>)
-}
+export const LoadingSpinner = ({size = Size.MD}: LoadingSpinnerProps) =>
+  (<LoadingSvg height={size} width={size} aria-label={ARIA_LABEL} role={ROLE} />)
+
 
 LoadingSpinner.Size = Size
