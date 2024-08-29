a41b679b1200a6de1c82b70dd00dba1764a5b9f2
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2024-08-29 11:08:02 -0700

    Updates about and newsletter strings

--- src/strings.ts
+++ src/strings.ts
@@ -11,9 +11,9 @@ export const navigation = {
 
 export const homeBio = {
   profileImgAlt: "Author Kimberly Dunham",
-  heading: "About Kimberly",
+  heading: "About me",
   aboutKimBody:
-    "Kimberly is a YA fantasy author and an Aurora Contest finalist. She lives in the rainy Pacific Northwest with her husband and daughter. Previously, she was a social media coordinator for a nonprofit women’s clinic. Today, she balances writing with homeschooling her daughter and working toward completing a degree in English language and literature. When she has a little downtime, which is never, she enjoys forest hikes, collecting houseplants, trying to decorate cakes, gardening, and playing guitar.",
+    "I am a YA fantasy author and an Aurora Contest finalist. I live in the rainy Pacific Northwest with my husband and daughter. Previously, I was a social media coordinator for a nonprofit women’s clinic. Today, I balance writing with homeschooling my daughter and working toward an English language and literature degree. When I have a little downtime, which is never, I enjoy forest hikes, collecting houseplants, trying to decorate cakes, gardening, and playing guitar.",
   moreLinkLabel: "read more...",
 };
 
@@ -32,7 +32,7 @@ export const about = {
 export const newsletter = {
   heading: "Newsletter signup",
   subheading:
-    "Subscribe to get full access to Kimberly's newsletter, which includes monthly writing updates, NaNoWriMo progress, publication news, contests she enters, and more.",
+    "Subscribe to get full access to my newsletter, which includes monthly writing updates, NaNoWriMo progress, publication news, contests I enter, and more.",
   emailLabel: "Email",
   signupBtnLabel: "Sign\u00A0up", // Unicode for character entity &nbsp;
   signupSuccess:
