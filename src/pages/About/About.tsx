4fdd93c37da4354e339db893c2a706046131ca44
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2024-10-21 23:48:30 -0700

    Updates all pages to use new PageWrapper for base styling

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,18 +1,18 @@
 import { AboutContent } from "../../components/Sections/AboutContent";
+import { PageWrapper } from "../../components/PageWrapper";
 import { Photo } from "../../components/Photo";
 import { about } from "../../strings";
-import classes from "./About.module.css";
 import forestStump from "../../assets/images/forest_stump_square.jpg";
 import kDunhamCoast from "../../assets/images/kdunham_oregon_coast_square.jpg";
 import kodaWriting from "../../assets/images/k_koda_square.jpg";
 
 export const About = () => {
   return (
-    <main className={classes.contentBody}>
+    <PageWrapper>
       <AboutContent heading={about.aboutHeading} text={about.aboutText}>
         <Photo
           src={kDunhamCoast}
-          alt={""}
+          alt={""} //TODO Add alt img text once image is finalized
           height="300"
           width="300"
           alignment={Photo.Align.Left}
@@ -21,7 +21,7 @@ export const About = () => {
       <AboutContent heading={about.forestsHeading} text={about.forestsText}>
         <Photo
           src={forestStump}
-          alt={""}
+          alt={""} //TODO Add alt img text once image is finalized
           height="300"
           width="300"
           alignment={Photo.Align.Right}
@@ -30,12 +30,12 @@ export const About = () => {
       <AboutContent heading={about.writingHeading} text={about.writingText}>
         <Photo
           src={kodaWriting}
-          alt={""}
+          alt={""} //TODO Add alt img text once image is finalized
           height="300"
           width="300"
           alignment={Photo.Align.Left}
         />
       </AboutContent>
-    </main>
+    </PageWrapper>
   );
 };
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,14 +1,25 @@
 import { ContactForm } from "../../components/Sections/ContactForm/";
+import { PageWrapper } from "../../components/PageWrapper";
+import { SectionWrapper } from "../../components/SectionWrapper";
 import { SocialIconSize } from "../../consts";
 import { Socials } from "../../components/Socials";
-import classes from "./Contact.module.css";
 import { contactForm } from "../../strings";
 
 export const Contact = () => {
   return (
-    <main className={classes.contentBody}>
+    <PageWrapper>
       <ContactForm />
-      <Socials size={SocialIconSize.Large} headingText={contactForm.socialsSubheading} showLabels />
-    </main>
+      <SectionWrapper
+        direction={SectionWrapper.Direction.Column}
+        backgroundColor={SectionWrapper.BackgroundColor.Transparent}
+        paddingCollapse
+      >
+        <Socials
+          size={SocialIconSize.Large}
+          headingText={contactForm.socialsSubheading}
+          showLabels
+        />
+      </SectionWrapper>
+    </PageWrapper>
   );
 };
