74bcc652226079ff017d458a97d580e623b913ba
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2024-10-24 15:59:10 -0700

    Adds getReleaseTextByDate util

--- src/utils.ts
+++ src/utils.ts
@@ -14,3 +14,21 @@ export const formatDateStr = (dateStr: string): string =>
     month: "short",
     day: "numeric",
   });
+
+export const getReleaseTextByDate = (dateStr: string) => {
+  const releaseDate = new Date(dateStr);
+  const today = new Date();
+  const oneYearAhead = new Date(today);
+  oneYearAhead.setFullYear(today.getFullYear() + 1);
+
+  if (releaseDate <= today) return `Published ${releaseDate.getFullYear()}`;
+
+  if (releaseDate > oneYearAhead) {
+    const month = releaseDate.toLocaleString("default", { month: "long" });
+    return `Coming ${month} ${releaseDate.getFullYear()}`;
+  }
+
+  const diffInTime = releaseDate.getTime() - today.getTime();
+  const diffInDays = Math.ceil(diffInTime / (1000 * 60 * 60 * 24));
+  return `Coming in ${diffInDays} days`;
+};
