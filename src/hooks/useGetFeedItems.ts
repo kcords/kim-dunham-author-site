e55d554b81f2c3e45a5654d905b0fe3de6c99a3d
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2024-10-24 23:00:22 -0700

    Adds stale and garbage collection times to useGetFeedItems

--- src/hooks/useGetFeedItems.ts
+++ src/hooks/useGetFeedItems.ts
@@ -1,5 +1,10 @@
+import { HOUR_IN_MILLISECONDS, MINUTE_IN_MILLISECONDS } from "../consts";
+
 import { fetchFeedItems } from "../api";
 import { useQuery } from "@tanstack/react-query";
+
+const TIME_TO_STALE = 20 * MINUTE_IN_MILLISECONDS;
+const TIME_TO_GARBAGE_COLLECT = 1 * HOUR_IN_MILLISECONDS;
 
 export const useGetFeedItems = () => {
   const {
@@ -10,6 +15,8 @@ export const useGetFeedItems = () => {
     queryKey: ["get-newsletter-feed-items"],
     queryFn: fetchFeedItems,
     initialData: [],
+    staleTime: TIME_TO_STALE,
+    gcTime: TIME_TO_GARBAGE_COLLECT,
   });
 
   return { isLoading, isError, feedItems };
